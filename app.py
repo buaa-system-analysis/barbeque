c9c40efb83f439a1da6e3f8057de664cd22bc137
Angus <1621xjx@buaa.edu.cn>
2019-04-18 22:01:11 +0800

    Update Flask API

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 import sys
 import json
 import os.path
-from UserControl import login, register, findUser, editInfo, changePassword
+from UserControl import login, register, find, editInfo, changePassword
 from PaperControl import purchase, download
 from ResourceControl import comment
 from ScholarControl import editScholarInfo, authenticate, manageResource
@@ -70,10 +70,6 @@ def user_register():
         userID = register(username=data['username'], password=data['password'], email=data['email'])
         if userID == 0:  # 注册失败
             code = 104
-        if userID == -100:  # 未找到用户名
-            code = 102
-        elif userID == -200:  # 密码错误
-            code = 103
         ans = {
             "code": code,  # 状态码
             "msg": "OK",
@@ -86,6 +82,278 @@ def user_register():
 
     return json.dumps(ans)
 
+
+@app.route("/api/user/find", methods=['POST'])
+def user_find():
+    data = request.form
+    try:
+        code = 100
+        user = find(userID=data['userID'])
+        if not user:
+            code = 105
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "user": user,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/user/edit_info", methods=['POST'])
+def user_edit_info():
+    data = request.form
+    try:
+        code = 100
+        flag = editInfo(userID=data['userID'], introduction=data['introduction'], organization='organization')
+        if not flag:
+            code = 106
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/user/change_pwd", methods=['POST'])
+def user_change_pwd():
+    data = request.form
+    try:
+        code = 100
+        flag = changePassword(userID=data['userID'], oldPassword=data['oldPassword'], newPassword='oldPassword')
+        if not flag:
+            code = 107
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/paper/purchase", methods=['POST'])
+def paper_purchase():
+    data = request.form
+    try:
+        code = 100
+        flag = purchase(userID=data['userID'], paperID=data['paperID'])
+        if not flag:
+            code = 201
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/user/download", methods=['POST'])
+def user_download():
+    data = request.form
+    try:
+        code = 100
+        url = download(paperID=data['paperID'])
+        if not url:
+            code = 202
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "url": url,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/resource/comment", methods=['POST'])
+def resource_comment():
+    data = request.form
+    try:
+        code = 100
+        flag = comment(userID=data['userID'], resourceID=data['resourceID'], content=data['content'])
+        if not flag:
+            code = 301
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/scholar/edit", methods=['POST'])
+def scholar_edit():
+    data = request.form
+    try:
+        code = 100
+        flag = editScholarInfo(scholarID=data['scholarID'], name=data['name'], organization=data['organization'], resourceField=data['resourceField'])
+        if not flag:
+            code = 401
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/scholar/auth", methods=['POST'])
+def scholar_auth():
+    data = request.form
+    try:
+        code = 100
+        flag = authenticate(userID=data['userID'], email=data['email'])
+        if not flag:
+            code = 402
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/scholar/manage", methods=['POST'])
+def scholar_manage():
+    data = request.form
+    try:
+        code = 100
+        flag = manageResource(resourceID=data['resourceID'], cmd=data['cmd'], newPrice=data['newPrice'])
+        if not flag:
+            code = 403
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/search/search", methods=['POST'])
+def search_search():
+    data = request.form
+    try:
+        code = 100
+        list = search(category=data['category'], keyword=data['keyword'])
+        if not list:
+            code = 501
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "list": list,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/collection/subscribe", methods=['POST'])
+def collection_subscribe():
+    data = request.form
+    try:
+        code = 100
+        flag = subscribe(userID=data['userID'], scholarID=data['scholarID'], cmd=data['cmd'])
+        if not flag:
+            code = 601
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/collection/paper", methods=['POST'])
+def collection_paper():
+    data = request.form
+    try:
+        code = 100
+        flag = collectPaper(userID=data['userID'], paperListID=data['paperListID'], cmd=data['cmd'], paperID=data['paperID'])
+        if not flag:
+            code = 602
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
+
+
+@app.route("/api/collection/manage", methods=['POST'])
+def collection_manage():
+    data = request.form
+    try:
+        code = 100
+        flag = manageCollection(userID=data['userID'], paperListID=data['paperListID'], cmd=data['cmd'], name=data['name'])
+        if not flag:
+            code = 603
+        ans = {
+            "code": code,  # 状态码
+            "msg": "OK",
+            "data": {
+                "flag": flag,
+            }
+        }
+    except Exception:
+        ans = error()
+
+    return json.dumps(ans)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
---
+++ docs/flask_api.md
@@ -0,0 +1,86 @@
+# Flask API
+
+## 启动说明
+
+默认服务端口为`0.0.0.0:5015`
+
+Run：
+
+```
+python app.py
+```
+
+可通过`0.0.0.0:5015/`对`template/main.html`进行访问
+
+## 接口说明
+
+*以下所有方法仅接受POST请求， 参数与返回值均以json形式传递，参数与返回值key的名称与类型必须与下表相同，接受返回值时请先判断状态码，再读返回数据*
+
+参数格式(json)：
+
+```json
+{
+    "key1": "参数1",
+    "key2": "参数2",
+    ...
+}
+```
+
+返回值格式(json)：
+
+```json
+{
+    "code": 200,  # 状态码（int） 
+    "msg": "OK",  # 信息（string），有OK与ERROR两种取值
+    "data": {
+        "key1": "参数1",
+        "key2": "参数2", 
+        ...
+    }
+}
+```
+
+| 函数名 | 地址 | 参数 | 返回值 | 状态码 |
+| --- | --- | --- | --- | --- |
+| user_login | /api/user/login | string username <br> string password | int userID | 100-正常 <br> 101-登录时出现其他错误 <br> 102-登录时未找到用户名 <br> 103-登录密码错误 |
+| user_register | /api/user/register | string username <br> string password <br> string email | int userID | 100-正常 <br> 104-注册失败 |
+| user_find | /api/user/find | int userID | dictionary user | 100-正常 <br> 105-用户查找失败 |
+| user_edit_info | /api/user/edit_info | int userID <br> string introduction <br> string organization | bool flag | 100-正常 <br> 106-用户信息编辑失败 |
+| user_change_pwd | /api/user/change_pwd | int userID <br> string oldPassword <br> string oldPassword | bool flag | 100-正常 <br> 107-用户修改密码失败 |
+| paper_purchase | /api/paper/purchase | int userID <br> int paperID | bool flag | 100-正常 <br> 201-文献购买失败 |
+| paper_download | /api/user/download | int paperID | string url | 100-正常 <br> 202-文献下载失败 |
+| resource_comment | /api/resource/comment | int userID <br> int resourceID <br> string content | bool flag | 100-正常 <br> 301-评论失败 |
+| scholar_edit | /api/scholar/edit | int scholarID <br> string name <br> string organization <br> string resourceField | bool flag | 100-正常 <br> 401-编辑学者失败 |
+| scholar_auth | /api/scholar/auth | int userID <br> string email | bool flag | 100-正常 <br> 402-学者认证失败 |
+| scholar_manage | /api/scholar/manage | int userID <br> int cmd <br> double newPrice | bool flag | 100-正常 <br> 403-资源管理操作失败 |
+| search_search | /api/search/search | string category <br> string keyword | list list[{"item": int, "scholar": int}, ...] | 100-正常 <br> 501-搜索论文失败 |
+| collection_subscribe | /api/collection/subscribe | int userID <br> int scholarID <br> bool cmd（关注为True，取关为False） | bool flag | 100-正常 <br> 601-关注/取关失败 |
+| collection_paper | /api/collection/paper | int userID <br> int paperListID <br> int cmd（1为添加，0为删除） <br> int paperID | bool flag | 100-正常 <br> 602-添加/收藏文献失败 |
+| collection_manage | /api/collection/manage | int userID <br> int paperListID <br> int cmd（1为添加文献列表，2为删除文献列表，3为收藏文献列表） <br> string name（）cmd不为1时，该字段为'' | bool flag | 100-正常 <br> 603-文献管理操作失败 |
+
+
+## 状态码汇总
+
+| 状态码 | 说明 |
+| --- | --- |
+| 0 | Flask错误，请联系xjx进行修复 |
+| 100 | 正常 |
+| 101 | 登录时出现其他错误 |
+| 102 | 登录时未找到用户名 |
+| 103 | 登录密码错误 |
+| 104 | 用户注册失败 |
+| 105 | 用户查找失败 |
+| 106 | 用户信息编辑失败 |
+| 107 | 用户修改密码失败 |
+| 201 | 文献购买失败 |
+| 202 | 文献下载失败 |
+| 301 | 评论失败 |
+| 401 | 编辑学者失败 |
+| 402 | 学者认证失败 |
+| 403 | 资源管理操作失败 |
+| 501 | 搜索论文失败 |
+| 601 | 关注/取关失败 |
+| 602 | 添加/收藏文献失败 |
+| 603 | 文献管理操作失败 |
+
+
